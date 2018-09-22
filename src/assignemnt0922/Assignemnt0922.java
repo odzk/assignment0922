fa0c43f89e01c7713b7e9f6b01af91a2cc5494b7
Odysseus Ambut <ody@web-mech.net>
2018-09-22 15:36:35 +0800

    initial upload

---
+++ src/assignemnt0922/Assignemnt0922.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignemnt0922;
+
+/**
+ *
+ * @author odzk
+ */
+public class Assignemnt0922 {
+
+    /**
+     * @param args the command line arguments
+     */
+//    public static void main(String[] args) {
+//        // TODO code application logic here
+//    }
+    
+}
